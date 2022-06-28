380f89dfb8ee6eb8586c2610fb64c600dab7f8fe
happi89 <farhaanp99@gmail.com>
2022-06-28 15:22:11 -0400

    added extra validation

--- script.js
+++ script.js
@@ -6,14 +6,44 @@ const messageContainer = document.querySelector('.message-container');
 const button = document.querySelector('button');
 
 let isVaild = false;
+let passwordsMatch = false;
 
 function validateForm() {
   // using contrainst api
   isVaild = form.checkValidity();
   // style message for an error
-  message.textContent = 'Please fill out all fields.';
-  message.style.color = 'red';
-  messageContainer.style.borderColor = 'red';
+
+  if(!isVaild) {
+    // show error message if form is not valid
+    message.textContent = 'Please fill out all fields.';
+    message.style.color = 'rgb(255, 58, 36)';
+    messageContainer.style.borderColor = 'rgb(255, 58, 36)';
+    return;
+  }
+
+  // check if passwords match
+  if(password1.value === password2.value) {
+    // style message for a success
+    passwordsMatch = true;
+    password1.style.borderColor = 'rgb(0, 255, 0)';
+    password2.style.borderColor = 'rgb(0, 255, 0)';
+  } else {
+    // if passwords don't match, style message for an error
+    passwordsMatch = false;
+    message.textContent = 'Passwords do not match.';
+    message.style.color = 'rgb(255, 58, 36)';
+    message.style.borderColor = 'rgb(255, 58, 36)';
+    password1.style.borderColor = 'rgb(255, 58, 36)';
+    password2.style.borderColor = 'rgb(255, 58, 36)';
+    return;
+  }
+  // if passwords match and form is valid
+  if(isVaild && passwordsMatch) {
+    // style message for a success
+    message.textContent = 'Registration successful!';
+    message.style.color = 'rgb(0, 255, 0)';
+    messageContainer.style.borderColor = 'rgb(0, 255, 0)';
+  }
 }
 
 //make sure passwords match
